4fb5fe0ce20661a1c4a6688a636f46e3f496543a
dineshchalla <s.s.r.challa@student.utwente.nl>
2026-07-15 18:39:34 +0200

    Our first successful connection to KSP.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use krpc_client::{services::space_center::SpaceCenter, Client};
+use krpc_client::error::RpcError;
+
+fn init_connection() -> Result<SpaceCenter, RpcError>{
+    let client = Client::new("ksp-mocr", "127.0.0.1", 50000, 50001).unwrap();
+
+    let space_center = SpaceCenter::new(client.clone());
+
+    let ship = space_center.get_active_vessel()?;
+
+    match ship.get_crew()?.first() {
+        Some(kerbal) => println!(
+            "hello, {}. Welcome to {}",
+            kerbal.get_name()?,
+            ship.get_name()?
+        ),
+        None => println!("{} is unkerbaled!", ship.get_name()?),
+    };
+    Ok(space_center)
+}
 fn main() {
-    println!("Hello, ksp-mocr!");
+    let result = init_connection();
+    match result {
+        Ok(_) => println!("Okay!"),
+        Err(a) => println!("{}", a.to_string())
+    }
 }
